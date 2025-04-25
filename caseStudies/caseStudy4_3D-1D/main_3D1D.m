495529d1b85ec8faf8a6598ed8e9d718a26530c5
CheikhSleiman <hani.cheikh.sleiman@outlook.com>
2025-04-25 08:15:06 +0200

    Update 3D-1D script

--- caseStudies/caseStudy4_3D-1D/main_3D1D.m
+++ caseStudies/caseStudy4_3D-1D/main_3D1D.m
@@ -8,15 +8,29 @@ markerSize=40;
 lineWidth=3;
 cMap=blood(250);
 
-%% Adding path to Tube2FEM core directory
+%% Adding Tube2FEM src directories to path
 CurrentFolder = pwd;
 TopFolder = fileparts(pwd);
 TopTopFolder = fileparts(fileparts(pwd));
-coreFolder = strcat(TopTopFolder,'\core');
 FEFolder = strcat(CurrentFolder,'\FiniteElement');
-addpath(coreFolder);
 
-%% Read .stl file
+srcFolder = strcat(TopTopFolder,'\src');
+finiteElementFolder = strcat(srcFolder,'\FiniteElement');
+boundaryConditionsFolder = strcat(srcFolder,'\boundaryConditions');
+postprocessingParaViewFolder = strcat(srcFolder,'\postprocessingParaView');
+surfaceMeshProcessingFolder = strcat(srcFolder,'\surfaceMeshProcessing');
+volumetricMeshFolder = strcat(srcFolder,'\volumetricMesh');
+skeletonisationFolder = strcat(srcFolder,'\skeletonisation');
+
+addpath(srcFolder);
+addpath(finiteElementFolder);
+addpath(boundaryConditionsFolder);
+addpath(postprocessingParaViewFolder);
+addpath(surfaceMeshProcessingFolder);
+addpath(volumetricMeshFolder);
+addpath(skeletonisationFolder);
+
+%% Read .stl file (this file was previously generated in caseStudy 3)
 fileName = 'Input/fullNetworkOptimised.stl';
 [stlStruct] = import_STL(fileName);
 
@@ -25,7 +39,6 @@ Vsurf=stlStruct.solidVertices{1};
 
 % Merging nodes (nodes are not merged in stl)
 [Fsurf,Vsurf]=mergeVertices(Fsurf,Vsurf);
-
 
 % Create Box
 boxDim=[650 620 300]; %Width in each direction
@@ -52,18 +65,28 @@ axisGeom(gca,fontSize);
 xlabel('X (\mum)')
 ylabel('Y (\mum)')
 zlabel('Z (\mum)')
+zoom(1.1)
 %print(gcf,'Surfaces.png','-dpng','-r600');
-%% Join surface sets
+
+%% Join Element sets (Network + Box)
+% multiply by a scaling factor
 Vbox = Vbox*1e-6;
 Vsurf = Vsurf*1e-6;
+
+% Join Vertices and Faces
 [F,V,C]=joinElementSets({Fbox,Fsurf},{Vbox,Vsurf});
+
+% Update labels
 C(1:size(faceBoundaryMarker(:),1))=faceBoundaryMarker;
-C(size(faceBoundaryMarker(:),1)+1:size(C,1)) = 7*ones((size(C,1)-size(faceBoundaryMarker,1)),1);
+C(size(faceBoundaryMarker(:),1)+1:size(C,1)) = 7*ones((size(C,1)- ...
+    size(faceBoundaryMarker,1)),1);
 
 % Find interior points
 [V_region1]=getInnerPoint({Fbox,Fsurf},{Vbox,Vsurf});
 [V_region2]=getInnerPoint(Fsurf,Vsurf);
 
+% Only |Box-Network| region is considered
+% Net region is considered a hole
 V_regions=V_region1;
 
 % Volume parameters
@@ -82,8 +105,10 @@ inputStruct.Faces=F; %Boundary faces
 inputStruct.Nodes=V; %Nodes of boundary
 inputStruct.faceBoundaryMarker=C;
 inputStruct.regionPoints=V_regions; %Interior points for regions
-inputStruct.holePoints=[V_region2; [475e-6 288e-6 180e-6]]; %Interior points for holes
-inputStruct.regionA=regionTetVolumes; %Desired tetrahedral volume for each region
+% the microvascular network consist of 2 detached sub-networks
+% therefore, we need 2 hole points
+inputStruct.holePoints=[V_region2; [475e-6 288e-6 180e-6]]; 
+inputStruct.regionA=regionTetVolumes; %Desired tet volume for each region
 inputStruct.modelName = modelName;
 
 % Mesh model using tetrahedral elements using tetGen
@@ -96,6 +121,7 @@ CE=meshOutput.elementMaterialID; %Element material or region id
 Fb=meshOutput.facesBoundary; %The boundary faces
 Cb=meshOutput.boundaryMarker; %The boundary markers
 
+% Update Element Material ID (label)
 meshOutput.elementMaterialID = ones(size(CE,1),1);
 
 %% Visualization
@@ -107,7 +133,7 @@ meshView(meshOutput,optionStruct);
 axisGeom(gca,fontSize);
 gdrawnow;
 axis off
-
+zoom(1.1)
 %print(gcf,'Surfaces.png','-dpng','-r600');
 
 %% Write version 2 ASCII .msh mesh format 
@@ -115,8 +141,7 @@ GmshFileName = 'Secomb3D1DGmshFormat.msh';
 [status] = writeGmsh(GmshFileName,meshOutput);
 
 
-
-%% Converst .msh to .xdmf via meshio
+%% Convert .msh to .xdmf via meshio
 meshPath = strcat(CurrentFolder,'\Mesh');
 meshPath = strrep(meshPath, '\', '/');
 % Split the path into parts
@@ -124,6 +149,8 @@ pathParts = strsplit(meshPath, '/');
 % Reconstruct the path without the first directory
 newMeshPath = strjoin(pathParts(2:end), '/'); 
 rootPath = "../../../../../../../../../../../../../../../../../mnt/c/";
+
+% Get wsl mesh path
 wslMeshPath = strcat(rootPath,newMeshPath,"/meshioConvertMesh.py");
 wslMeshPath= sprintf('"%s"', wslMeshPath);
 
@@ -135,8 +162,9 @@ GmshFileName= sprintf('"%s"', GmshFileName);
 fprintf(fid1,strcat('GmshFileName=',GmshFileName));
 fclose(fid1);
 
+% Combine two scripts (mesh info & general conversion script in src)
 script1 = fileread('meshInfo.txt');
-script2 = fileread('../../../core/ConvertGmshToXdmf.py');
+script2 = fileread('../../../src/volumetricMesh/ConvertGmshToXdmf.py');
 
 fid2 = fopen('meshioConvertMesh.py','wt');
 fprintf(fid2,script1);
@@ -152,7 +180,7 @@ runString = strcat(wslPath,' python3',append(' ',wslMeshPath));
 cd ../simOutput_1D
 
 %% Read Simulation Results
-
+% Reading 1D Finite Volume simulation outputs
 field1 = xlsread('@1.xlsx');
 field2 = xlsread('@2.xlsx');
 field3 = xlsread('@3.xlsx');
@@ -160,8 +188,7 @@ field4 = xlsread('@4.xlsx');
 field5 = xlsread('@5.xlsx');
 field6 = xlsread('@6.xlsx');
 
-
-% Organise data differnetly
+% Organise data differently
 vertexCoor = field1;
 connectivity = field2;
 m = size(connectivity,1);
@@ -209,14 +236,20 @@ end
 
 [E,Vske]=joinElementSets(CellArray_E,CellArray_V);
 [E,Vske,ind1]=mergeVertices(E,Vske);
+
+% Shifting network in the (x,y) plane by 30 units in each direction
 Vske = Vske + 30*[ones(size(Vske,1),1),ones(size(Vske,1),1),zeros(size(Vske,1),1)];
+% Multiplying by a scaling factor
 Vske = Vske*10^-6;
 
+% Reading Oxygen value from 1D simulation
 oxygenSke = CellArray_O{1,1};
 for i = 2:m
     oxygenSke = [oxygenSke; CellArray_O{1,i}];
 end
 oxygenSke=oxygenSke(ind1);
+
+% Unit conversion 
 oxygenSke=oxygenSke*4.2e-6/80;
 
 %% Visualisation
@@ -228,6 +261,7 @@ camlight headlight;
 drawnow; 
 gpatch(E,Vske,'none',oxygenSke,0,3);
 axis off
+zoom(1.3)
 %print(gcf,'1Dsimulation.png','-dpng','-r600');
 % Visualisation of the surface mesh and the 1D network
 cFigure; 
@@ -238,21 +272,24 @@ hold on
 gpatch(E,Vske,'none',oxygenSke,0,3);
 drawnow
 axis off
+zoom(1.3)
 % print(gcf,'1DsimulationInSTL.png','-dpng','-r600');
-%% Projection back from 1D bars to 3D Surface Mesh
-[D,minInd]=minDist(Vsurf,Vske); %minInd is the index of the vertices in the skeleton
-%if C was a pressure field on the skeleton, C2 is the Pressure on the 3D vessel wall
+%% Projection back from 1D segments to 3D Surface Mesh
+%minInd is the index of the vertices in the skeleton
+[D,minInd]=minDist(Vsurf,Vske); 
 oxygenSurf = oxygenSke(minInd);
 
 %Visualisation
 [oxygenSurfFaces]=vertexToFaceMeasure(Fsurf,oxygenSurf);
 faceAlpha=1;
 cFigure;
-patch('faces',Fsurf,'vertices',Vsurf,'FaceColor','flat','CData',oxygenSurfFaces,'FaceAlpha',faceAlpha,'EdgeColor','none');
+patch('faces',Fsurf,'vertices',Vsurf,'FaceColor','flat', ...
+    'CData',oxygenSurfFaces,'FaceAlpha',faceAlpha,'EdgeColor','none');
 axisGeom;
 camlight ;
 drawnow;
 axis off
+zoom(1.3)
 % print(gcf,'BC.png','-dpng','-r600');
 
 %% Exporting Vsurf and oxygenSurf
@@ -291,7 +328,7 @@ rootPath = "../../../../../../../../../../../../../../../../../mnt/c/";
 wslFEPath = strcat(rootPath,newFEPath,"/FEniCS_3D-1D.py");
 wslFEPath= sprintf('"%s"', wslFEPath);
 
-% ----Create Infor.txt -----%
+% ----Create Info.txt -----%
 fid1 = fopen('Info.txt','wt');
 meshPath1 = sprintf('"%s"', strcat(rootPath,newMeshPath,"/Tetra.xdmf"));
 meshPath2 = sprintf('"%s"', strcat(rootPath,newMeshPath,"/Tri.xdmf"));
@@ -312,18 +349,15 @@ fprintf(fid1,'\n');
 fclose(fid1);
 
 script1 = fileread('Info.txt');
-script2 = fileread('../../../core/FEniCS_3D-1D.py');
+script2 = fileread('../../../src/FiniteElement/FEniCS_3D-1D.py');
 
-% Join Info.txt with 3D-1D FEniCS file in the core
+% Join Info.txt with 3D-1D FEniCS file in the src/FiniteElement
 fid2 = fopen('FEniCS_3D-1D.py','wt');
 fprintf(fid2,script1);
 fprintf(fid2,'\n');
 fprintf(fid2,script2);
 fprintf(fid2,'\n');
 fclose(fid2);
-
-
-
 
 % Run FEniCS_3D-1D in the case Study directory
 wslPath = '"C:\Windows\System32\wsl.exe"';
